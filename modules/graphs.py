437c630f01bc2a60a944e2fd4a15334abf49a91d
TheAmalLalgi <amallalgi2487@gmail.com>
2022-09-28 19:34:28 -0700

    add some wallpapers �️

--- modules/graphs.py
+++ modules/graphs.py
@@ -26,3 +26,20 @@ def histogram(x_axis, y_axis) :
 	plt.title('Class strengths graph - Histogram')
 	plt.grid(True)
 	plt.show()
+
+
+# raw_data = {'first_name': ['Sheldon', 'Raj', 'Leonard', 'Howard', 'Amy'],
+            'last_name': ['Copper', 'Koothrappali', 'Hofstadter', 'Wolowitz', 'Fowler'],
+            'age': [42, 38, 36, 41, 35],
+            'Comedy_Score': [9, 7, 8, 8, 5],
+            'Rating_Score': [25, 25, 49, 62, 70]
+            }
+# df = pd.DataFrame(raw_data)
+# print('Dataframe: \n', df)
+
+# searchInput = input('\nSEARCH: Enter a first name: ')
+
+# print('\nwhere :\n')
+# dfSearch = df.where(df['first_name'] == searchInput)
+# dfSearch = dfSearch.dropna()
+# print(dfSearch)
